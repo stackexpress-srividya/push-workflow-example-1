d39fc3babb6d4f8b70126b1a25a0aad3cebf171d
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-06-19 21:21:41 +0100

    adding version to log

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"net/http"
 )
 
+var version = "v0"
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website! Version 0")
+		fmt.Fprintf(w, "Welcome to my website! Version %s", version)
 	})
+	fmt.Printf("App is starting, version: %s \n", version)
 	log.Fatal(http.ListenAndServe(":8500", nil))
 }
